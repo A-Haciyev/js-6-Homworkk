7f647cb5f4974dfd93bdded9b27741dfe049dc42
A-Haciyev <ayazbjk2009@gmail.com>
2025-05-05 18:56:24 +0400

    salam

---
+++ index.js
@@ -0,0 +1,18 @@
+console.log('Task 1');
+function greeting() {
+    console.log('Ayaz is a JS developer');
+    
+}
+greeting();
+console.log("Task 2");
+function message (username) {
+    console.log(username + " " + "is active now");
+}
+message("Adnan");
+message("Behram");
+message("Ilham");
+console.log("Task 3");
+function number(number1 = 444 , number2 = 555 , number3 = 6666) {
+    console.log(number3);
+}
+number();
